783a1b62e77cddbdb14831cb2297f113bb130b46
a-poduval <ashwin.poduval@gmail.com>
2023-10-20 11:47:07 -0500

    Record max time elapsed among all threads for worst case vs average bandwidth calculations

--- coalesce-bytes.sh
+++ coalesce-bytes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Name,Function,Time Elapsed (ns),Read Bytes,Written Bytes,Total Instructions,Counter Instructions,Counter Increments Contributing to Overhead" > zray-byte-stats.csv
+echo "Name,Function,Time Elapsed (ns),Max Thread Time Elapsed,Read Bytes,Written Bytes,Total Instructions,Counter Instructions,Counter Increments Contributing to Overhead" > zray-byte-stats.csv
 
 kernels=("bc" "bfs" "cc" "cc_sv" "pr" "pr_spmv" "sssp" "tc")
 
@@ -12,10 +12,11 @@ for i in $(seq 0 7); do
     cd $BASE_DIR
     cd ${kernels[$i]}
     TIME_ELAPSED=$(grep "TIME ELAPSED (ns)" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
+    MAX_THREAD_TIME=$(grep "TIME ELAPSED (ns)" tool_log_file.txt | grep -o '\w*$' | sort -n | tail -1)
     READ_BYTES=$(grep "READ BYTES" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
     WRITE_BYTES=$(grep "WRITTEN BYTES" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
     TOTAL_INST=$(grep "TOTAL INST" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
     CNTR_INST=$(grep "COUNTER INST" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
     OVERHEAD=$(grep "OVERHEAD INCREMENTS" tool_log_file.txt | grep -o '\w*$' | paste -sd+ - | bc)
-    echo ${kernels[$i]},${functions[$i]},$TIME_ELAPSED,$READ_BYTES,$WRITE_BYTES,$TOTAL_INST,$CNTR_INST,$OVERHEAD >> ../zray-byte-stats.csv
+    echo ${kernels[$i]},${functions[$i]},$TIME_ELAPSED,$MAX_THREAD_TIME,$READ_BYTES,$WRITE_BYTES,$TOTAL_INST,$CNTR_INST,$OVERHEAD >> ../zray-byte-stats.csv
 done
